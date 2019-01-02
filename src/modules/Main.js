9904e1797db7f0054a98e30a3513e0a9f5ab747d
Paul Naranja <pnaranja@chegg.com>
2019-01-02 00:48:31 -0800

    Try inline styles and using js in jsx

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,12 +1,21 @@
 import React from "react"
 
-const Main = ()=>(
-    <ul>
-        <div> <input type="checkbox"/> Placeholder </div>
-        <div> <input type="checkbox"/> Placeholder </div>
-        <div> <input type="checkbox"/> Placeholder </div>
-        <div> <input type="checkbox"/> Placeholder </div>
-    </ul>
-)
+const Main = () => {
+    const GreenBlue = 'Green or Blue!';
+    const date = new Date();
+
+    const one_style = date.getSeconds() <= 30 ? {color: '#0000FF'} : {color: '#008000'};
+
+    return (
+        <ul>
+            <div>Today is {date.toTimeString()}</div>
+            <div style={one_style}><input type="checkbox"/> {`${GreenBlue}`} </div>
+            <div><input type="checkbox"/> Placeholder</div>
+            <div><input type="checkbox"/> Placeholder</div>
+            <div><input type="checkbox"/> Placeholder</div>
+        </ul>
+    )
+
+};
 
 export default Main
